475e301791dee660d3346ebf520d3df4d03251e8
furaihan <28698437+furaihan@users.noreply.github.com>
2022-12-31 15:26:12 +0700

    working navigation

--- View Model/MainViewModel.cs
+++ View Model/MainViewModel.cs
@@ -3,13 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NOTE_ID.View_Model
 {
     internal class MainViewModel : BaseViewModel
     {
-        public CommandViewModel HomeViewCommand { get; set; }
-        public CommandViewModel ReservationViewCommand { get; set; }
-        public CommandViewModel HistoryViewCommand { get; set; }
+        private ICommand homeViewCommand;
+        private ICommand fAQViewCommand;
+        private ICommand aboutViewCommand;
+        private ICommand loginViewCommand;
+        private object currentView;
+        private AboutPageViewModel aboutPage;
+        private FAQPageViewModel fAQPage;
+        private HomePageViewModel homePage;
+        private LoginPageViewModel loginPage;
+
+        public ICommand HomeViewCommand
+        {
+            get => homeViewCommand;
+            set => homeViewCommand = value;
+        }
+        public ICommand FAQViewCommand
+        {
+            get => fAQViewCommand;
+            set => fAQViewCommand = value;
+        }
+        public ICommand AboutViewCommand
+        {
+            get => aboutViewCommand;
+            set => aboutViewCommand = value;
+        }
+        public ICommand LoginViewCommand
+        {
+            get => loginViewCommand; set => loginViewCommand = value;
+        }
+        public object CurrentView
+        {
+            get => currentView;
+            set
+            {
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public HomePageViewModel HomePage
+        {
+            get => homePage;
+            set
+            {
+                homePage = value;
+                OnPropertyChanged();
+            }
+        }
+        public AboutPageViewModel AboutPage
+        {
+            get => aboutPage;
+            set
+            {
+                aboutPage = value;
+                OnPropertyChanged();
+            }
+        }
+        public FAQPageViewModel FAQPage
+        {
+            get => fAQPage;
+            set
+            {
+                fAQPage = value;
+                OnPropertyChanged();
+            }
+        }
+        public LoginPageViewModel LoginPage
+        {
+            get => loginPage;
+            set
+            {
+                loginPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            homePage = new HomePageViewModel();
+            aboutPage = new AboutPageViewModel();
+            fAQPage = new FAQPageViewModel();
+            loginPage = new LoginPageViewModel();
+
+            currentView = homePage;
+
+            homeViewCommand = new CommandViewModel(x => CurrentView = homePage);
+            aboutViewCommand = new CommandViewModel(x => CurrentView = aboutPage);
+            fAQViewCommand = new CommandViewModel(x => CurrentView = fAQPage);
+            loginViewCommand = new CommandViewModel(x => CurrentView = loginPage);
+        }
+
     }
 }
