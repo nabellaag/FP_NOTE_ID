cecb66a0fe693a8315446080c8acb597e0f6458a
nabellaag <nabellaayug@students.amikom.ac.id>
2023-01-07 16:09:06 +0700

    badsb

--- Model/ReadingList.cs
+++ Model/ReadingList.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace NOTE_ID.Model
 {
-    internal class ReadingList
+    public class ReadingList
     {
+        public string TitleBook { get; set; }
+        public DateTime Tanggal { get; set; }
+        public string Type { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+
+
+        public enum status
+        {
+            finished = 0,
+            notYet = 1
+        }
     }
 }
